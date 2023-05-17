dbee16252e0167f4673f2998452c4287c35763fb
TestFa7aL92 <109145836+TestFa7aL92@users.noreply.github.com>
2023-05-17 20:21:49 +0200

    d11f

--- TestGit/src/main/java/org/example/JakasKlasa2.java
+++ TestGit/src/main/java/org/example/JakasKlasa2.java
@@ -4,4 +4,7 @@ public class JakasKlasa2 {
     public void asd(){
         System.out.println("jestem adam i jestem gejem");
     }
+    public void qwe(){
+        System.out.println("cipa");
+    }
 }
