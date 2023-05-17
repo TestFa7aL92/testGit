81170276ba6e6f615b1e774426bc00034896d96b
TestFa7aL92 <109145836+TestFa7aL92@users.noreply.github.com>
2023-05-17 20:03:53 +0200

    commit 23123123123123

--- TestGit/src/main/java/org/example/Jaszczur1337.java
+++ TestGit/src/main/java/org/example/Jaszczur1337.java
@@ -4,4 +4,7 @@ public class Jaszczur1337 {
     public void dqw(){
         System.out.println("pjoter lubi lizać męskie pały");
     }
+    public void palka(){
+        System.out.println("wszystkie geje to pedały");
+    }
 }
